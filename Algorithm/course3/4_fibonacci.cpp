5b238e33865364356428b6c0e5289f69c3d713b5
JISU SHIN <csjisu2009@gmail.com>
2018-09-16 22:24:53 +0900

    fibonacci

--- Algorithm/course3/4_fibonacci.cpp
+++ Algorithm/course3/4_fibonacci.cpp
@@ -1,16 +1,25 @@
+//time   : 
+
+//review : you should use memoization
+
 #include <stdio.h>
+#include <string.h>
+
+int cache[45+1];
 
 int fibo(int n){
     if(n == 0) return 0;
     if(n == 1) return 1;
 
-    return fibo(n-1) + fibo(n-2);
+	int& ret = cache[n];
+	if(ret != -1) return ret;
+	return ret = fibo(n-1) + fibo(n-2);
 }
 
 int main(){
+	memset(cache, -1, sizeof(cache));
     int n;
     scanf("%d", &n);
-
     printf("%d\n", fibo(n));
 
     return 0;
