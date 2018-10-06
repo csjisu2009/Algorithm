1f4d1c6eb06bb0854531c343fa883ffbc5c6d12c
JISU SHIN <csjisu2009@gmail.com>
2018-10-06 18:53:36 +0900

    DessertSolved

---
+++ Algorithm/course4/6_danji.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define MAX 25
+int N;
+int count = 0;
+int board[MAX][MAX];
+bool boardDefined[MAX][MAX];
+int definedDanji[13*13];
+
+void defineDanjiNum(int y, int x){
+    if(y < 0 || N <= y) return;
+    if()
+}
+
+void searchMap(){
+
+}
+
+int main(){
+    return 0;
+}
