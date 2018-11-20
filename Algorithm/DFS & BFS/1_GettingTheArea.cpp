cc91c37cdf144b20d9e2808731275b0e76e1bec3
JISU SHIN <csjisu2009@gmail.com>
2018-11-20 22:02:38 +0900

    GettingTheAreaSolved

--- Algorithm/DFS & BFS/1_GettingTheArea.cpp
+++ Algorithm/DFS & BFS/1_GettingTheArea.cpp
@@ -2,139 +2,143 @@
              you must think clearly when use dfs and when use bfs
              you know when dfs and bfs are same same?? -- when you wanna check all the situation!! they are same same!!
 */
-
-//time   : 1h 30m
-
-/*review : 1. bfs !!
-           2. why 60.0 -> 50.0 after solving the problem of
-            input:
-              2 2 3
-              0 0 0 0
-              0 1 0 1
-              1 0 1 0
-            output:
-              4 -> 1
+ 
+//time   : 2h 0m
+ 
+/*review : 1. bfs !!(board[][] or bDiscovered[][].. just choose one!!)
+           2. think out of the box!! think about a "line"!!(not a "square")
+           3. boundary bug in bfs!!
+               0 < vp.y-1
+               0 < vp.x-1
+               
+               testCase:
+               input:
+               3 2 3
+               0 1 1 2
+               0 1 0 1
+               0 1 0 2
+               
+               output:
+               1
+               5
 */
-
+ 
 #include <stdio.h>
 #include <queue>
 using namespace std;
-
+ 
 #define MAX 100
-
+ 
 int Y, X, K;
-//int board[MAX][MAX]; //you do not have to use board[][]!! because you already have bDiscovered[][]!!
 class Point {
 public:
-	int y;
-	int x;
-
-	Point() {}
-	Point(int y, int x) {
-		this->y = y;
-		this->x = x;
-	}
+    int y;
+    int x;
+ 
+    Point() {}
+    Point(int y, int x) {
+        this->y = y;
+        this->x = x;
+    }
 };
-bool bDiscovered[MAX][MAX];
-int sArray[MAX];
+bool bDiscovered[MAX+1][MAX+1];
+int sArray[MAX*MAX];
 int sIndex = 0;
 int s = 0;
 void bfs(Point p) {
-	bDiscovered[p.y][p.x] = true;
-	s++;
-	queue<Point> qNotVistedYet;
-	qNotVistedYet.push(p);
-	Point vp;
-	while (!qNotVistedYet.empty()) {
-		vp = qNotVistedYet.front();
-		qNotVistedYet.pop();
-
-		//check Up-Coordinate
-		if (vp.y + 1 < Y && !bDiscovered[vp.y + 1][vp.x]) {
-			bDiscovered[vp.y + 1][vp.x] = true;
-			qNotVistedYet.push(Point(vp.y+1, vp.x));
-			s++;
-		}
-		//check Right-Coordinate
-		if (vp.x + 1 < X && !bDiscovered[vp.y][vp.x+1]) {
-			bDiscovered[vp.y][vp.x+1] = true;
-			qNotVistedYet.push(Point(vp.y, vp.x+1));
-			s++;
-		}
-		//check Down-Coordinate
-		if (0 < vp.y-1 && !bDiscovered[vp.y-1][vp.x]) {
-			bDiscovered[vp.y-1][vp.x] = true;
-			qNotVistedYet.push(Point(vp.y-1, vp.x));
-			s++;
-		}
-		//check Left-Coordinate
-		if (0 < vp.x-1 && !bDiscovered[vp.y][vp.x-1]) {
-			bDiscovered[vp.y][vp.x-1] = true;
-			qNotVistedYet.push(Point(vp.y, vp.x-1));
-			s++;
-		}
-	}
-	sArray[sIndex] = s;
-	s = 0;
-	sIndex++;
-	return;
+    bDiscovered[p.y][p.x] = true;
+    s++;
+    queue<Point> qNotVistedYet;
+    qNotVistedYet.push(p);
+    Point vp;
+    while (!qNotVistedYet.empty()) {
+        vp = qNotVistedYet.front();
+        qNotVistedYet.pop();
+ 
+        //check Up-Coordinate
+        if (vp.y + 1 < Y && !bDiscovered[vp.y + 1][vp.x]) {
+            bDiscovered[vp.y + 1][vp.x] = true;
+            qNotVistedYet.push(Point(vp.y+1, vp.x));
+            s++;
+        }
+        //check Right-Coordinate
+        if (vp.x + 1 < X && !bDiscovered[vp.y][vp.x+1]) {
+            bDiscovered[vp.y][vp.x+1] = true;
+            qNotVistedYet.push(Point(vp.y, vp.x+1));
+            s++;
+        }
+        //check Down-Coordinate
+        if (0 <= vp.y-1 && !bDiscovered[vp.y-1][vp.x]) {//bug : 0 < vp.y-1
+            bDiscovered[vp.y-1][vp.x] = true;
+            qNotVistedYet.push(Point(vp.y-1, vp.x));
+            s++;
+        }
+        //check Left-Coordinate
+        if (0 <= vp.x-1 && !bDiscovered[vp.y][vp.x-1]) {//bug : 0 < vp.x-1
+            bDiscovered[vp.y][vp.x-1] = true;
+            qNotVistedYet.push(Point(vp.y, vp.x-1));
+            s++;
+        }
+    }
+    sArray[sIndex] = s;
+    s = 0;
+    sIndex++;
+    return;
 }
-
+ 
 //quick sort
-	//source code
 int partition(int* A, int start, int end) {
-	int pivot = A[end];
-	int pIndex = start;
-
-	for (int i = start; i < end; ++i)
-		if (A[i] <= pivot) {
-			swap(A[i], A[pIndex]);
-			++pIndex;
-		}
-	swap(A[pIndex], A[end]);
-	return pIndex;
+    int pivot = A[end];
+    int pIndex = start;
+ 
+    for (int i = start; i < end; ++i)
+        if (A[i] <= pivot) {
+            swap(A[i], A[pIndex]);
+            ++pIndex;
+        }
+    swap(A[pIndex], A[end]);
+    return pIndex;
 }
-
+ 
 void quickSort(int* A, int start, int end) {
-	if (start < end) {
-		int pIndex = partition(A, start, end);
-		quickSort(A, start, pIndex - 1);
-		quickSort(A, pIndex + 1, end);
-	}
+    if (start < end) {
+        int pIndex = partition(A, start, end);
+        quickSort(A, start, pIndex - 1);
+        quickSort(A, pIndex + 1, end);
+    }
 }
-
+ 
 int main() {
-	scanf("%d %d %d", &Y, &X, &K);
-	//initialize board[][] as 1
-	for(int j = 0 ; j < Y ; j++)
-		for (int i = 0; i < X; i++)
-			bDiscovered[j][i] = false;
-	//declare square's leftDown coordinates and rightUp coordinates
-	int yDown, xDown, yUp, xUp;
-	//fill the board's square area with 0
-	for (int i = 0; i < K; i++) {
-		scanf("%d %d %d %d", &xDown, &yDown, &xUp, &yUp);
-		if (xDown == xUp && yDown == yUp)//do not forget the 'square's s == 1' situation!!
-			bDiscovered[yUp][xUp] = true;
-		else {
-			for (int y = yDown; y < yUp; y++)
-				for (int x = xDown; x < xUp; x++)
-					bDiscovered[y][x] = true;
-		}
-	}
-
-	for(int j = 0 ; j < Y ; j++)
-		for (int i = 0; i < X; i++) {
-			if (!bDiscovered[j][i])
-				bfs(Point(j, i));
-		}
-
-	printf("%d\n", sIndex);
-	quickSort(sArray, 0, sIndex-1);//becareful!! [start, end], not [start, end)
-	for (int i = 0; i < sIndex; i++) {
-		printf("%d", sArray[i]);
-		if (i == sIndex - 1) printf("\n");
-		else printf(" ");
-	}
-	return 0;
+    scanf("%d %d %d", &Y, &X, &K);
+    //initialize board[][] as 1
+    for(int j = 0 ; j < Y ; j++)
+        for (int i = 0; i < X; i++)
+            bDiscovered[j][i] = false;
+    //declare square's leftDown coordinates and rightUp coordinates
+    int yDown, xDown, yUp, xUp;
+    //fill the board's square area with 0
+    for (int i = 0; i < K; i++) {
+        scanf("%d %d %d %d", &xDown, &yDown, &xUp, &yUp);
+        if(xDown == xUp || yDown == yUp) continue;//consider the "line" situation!!
+        for (int y = yDown; y < yUp; y++)
+            for (int x = xDown; x < xUp; x++)
+                bDiscovered[y][x] = true;
+    }
+     
+    for(int j = 0 ; j < Y ; j++)
+        for (int i = 0; i < X; i++) {
+            if (!bDiscovered[j][i])
+                bfs(Point(j, i));
+        }
+ 
+    printf("%d\n", sIndex);
+    quickSort(sArray, 0, sIndex-1);//becareful!! [start, end], not [start, end)
+    if(sIndex == 0) printf("0\n");
+    else
+        for (int i = 0; i < sIndex; i++) {
+            printf("%d", sArray[i]);
+            if (i == sIndex - 1) printf("\n");
+            else printf(" ");
+        }
+    return 0;
 }
--- Algorithm/course6/5_MazeSolving.cpp
+++ Algorithm/course6/5_MazeSolving.cpp
@@ -1,124 +1,132 @@
-/*remember : when you wannna solve shortest path problem, bfs is proper
-             you must think clearly when use dfs and when use bfs
-             you know when dfs and bfs are same same?? -- when you wanna check all the situation!! they are same same!!
-*/
-
-//time   : 2h 0m
-
-/*review : 1. bfs shortest path!!
-           2. reference: 
+//time   : 0h 0m
+ 
+/*review : 1. reference: 
               Maze solving algorithm - https://en.wikipedia.org/wiki/Maze_solving_algorithm
               A* search algorithm - https://en.wikipedia.org/wiki/A*_search_algorithm
 */
-
+ 
+//time   : 0h 42m
+ 
+/*review : 1. improve print \n part, exercise code more faster
+           2. bfs remember 3 status
+              1) discover-x
+              2) discover-o  visit-x
+              3) discover-o  visit-o
+           3. consider the base case!!
+              testCase:
+              input:
+                1 1
+                0
+              output:
+                0
+*/
 #include <stdio.h>
 #include <iostream>
 #include <queue>
 using namespace std;
-
+ 
 #define MAX 1000
-
+ 
 class Point {
 public:
-	int y;
-	int x;
-
-	Point() {}
-	Point(int _y, int _x) {
-		y = _y;
-		x = _x;
-	}
+    int y;
+    int x;
+ 
+    Point() {}
+    Point(int _y, int _x) {
+        y = _y;
+        x = _x;
+    }
 };
-
+ 
 int Y, X;
 int board[MAX][MAX];
 queue<Point> qPoints;
 bool bVisited[MAX][MAX];
-//bool bConnected[MAX][MAX];
 bool bDiscovered[MAX][MAX];// 1) discover-x
 Point prePoint[MAX][MAX];
-
+ 
 void bfs(int y, int x) {
-	bDiscovered[y][x] = true;// 2) discover-o
-	queue<Point> qNotVisitedYet;
-	Point pNVY(y, x);
-	qNotVisitedYet.push(pNVY);// 2) visit-x
-	while (!qNotVisitedYet.empty()) {
-		Point vp = qNotVisitedYet.front();// 3) visit-o
-		y = vp.y;
-		x = vp.x;
-		//printf("Point( %d , %d )\n", vp.y, vp.x);
-		//printf("queue size : %d\n", qNotVisitedYet.size());
-		qNotVisitedYet.pop();
-		
-		//UP : Point(y-1, x)
-		if ( ( 0 <= y-1 && y-1 < Y) && !bDiscovered[y-1][x] && board[y-1][x] == 0) {
-			Point pNVY(y-1, x);
-			qNotVisitedYet.push(pNVY);
-			bDiscovered[y-1][x] = true;
-			prePoint[y - 1][x] = Point(y, x);
-		}
-		//RIGHT : Point(y, x+1)
-		if ((0 <= x+1 && x+1 < X) && !bDiscovered[y][x+1] && board[y][x+1] == 0) {
-			Point pNVY(y, x+1);
-			qNotVisitedYet.push(pNVY);
-			bDiscovered[y][x+1] = true;
-			prePoint[y][x+1] = Point(y, x);
-		}
-		//DOWN : Point(y+1, x)
-		if ((0 <= y+1 && y+1 < Y) && !bDiscovered[y+1][x] && board[y+1][x] == 0) {
-			Point pNVY(y+1, x);
-			qNotVisitedYet.push(pNVY);
-			bDiscovered[y+1][x] = true;
-			prePoint[y+1][x] = Point(y, x);
-		}
-		//LEFT : Point(y, x-1)
-		if ((0 <= x-1 && x-1 < X) && !bDiscovered[y][x-1] && board[y][x-1] == 0) {
-			Point pNVY(y, x-1);
-			qNotVisitedYet.push(pNVY);
-			bDiscovered[y][x-1] = true;
-			prePoint[y][x-1] = Point(y, x);
-		}
-	}
-	return;
+    bDiscovered[y][x] = true;// 2) discover-o
+    queue<Point> qNotVisitedYet;
+    Point pNVY(y, x);
+    qNotVisitedYet.push(pNVY);// 2) visit-x
+    while (!qNotVisitedYet.empty()) {
+        Point vp = qNotVisitedYet.front();// 3) visit-o
+        y = vp.y;
+        x = vp.x;
+        //printf("Point( %d , %d )\n", vp.y, vp.x);
+        //printf("queue size : %d\n", qNotVisitedYet.size());
+        qNotVisitedYet.pop();
+        
+        //UP : Point(y-1, x)
+        if ( ( 0 <= y-1 && y-1 < Y) && !bDiscovered[y-1][x] && board[y-1][x] == 0) {
+            Point pNVY(y-1, x);
+            qNotVisitedYet.push(pNVY);
+            bDiscovered[y-1][x] = true;
+            prePoint[y - 1][x] = Point(y, x);
+        }
+        //RIGHT : Point(y, x+1)
+        if ((0 <= x+1 && x+1 < X) && !bDiscovered[y][x+1] && board[y][x+1] == 0) {
+            Point pNVY(y, x+1);
+            qNotVisitedYet.push(pNVY);
+            bDiscovered[y][x+1] = true;
+            prePoint[y][x+1] = Point(y, x);
+        }
+        //DOWN : Point(y+1, x)
+        if ((0 <= y+1 && y+1 < Y) && !bDiscovered[y+1][x] && board[y+1][x] == 0) {
+            Point pNVY(y+1, x);
+            qNotVisitedYet.push(pNVY);
+            bDiscovered[y+1][x] = true;
+            prePoint[y+1][x] = Point(y, x);
+        }
+        //LEFT : Point(y, x-1)
+        if ((0 <= x-1 && x-1 < X) && !bDiscovered[y][x-1] && board[y][x-1] == 0) {
+            Point pNVY(y, x-1);
+            qNotVisitedYet.push(pNVY);
+            bDiscovered[y][x-1] = true;
+            prePoint[y][x-1] = Point(y, x);
+        }
+    }
+    return;
 }
-
-int countPathLen(int startY, int startX, int endY, int endX) {
-	int cnt = 0;
-	int tempY = endY, tempX = endX;
-	int j, i;
-	
-	while (true) {
-		cnt++;
-		if (prePoint[tempY][tempX].y == startY
-			&& prePoint[tempY][tempX].x == startX)
-			break;
-		j = prePoint[tempY][tempX].y;
-		i = prePoint[tempY][tempX].x;;
-		tempY = j;
-		tempX = i;
-	}
-	return cnt;
-}
-
+ 
 int main() {
-	scanf("%d %d", &Y, &X);
-	for (int j = 0; j < Y; j++)
-		for (int i = 0; i < X; i++)
-			scanf("%d", &board[j][i]);
-
-	for(int j = 0 ; j < Y ; j++)
-		for (int i = 0; i < X; i++) {
-			bDiscovered[j][i] = false;
-			bVisited[j][i] = false;
-		}
-
-	//convert maze to graph
-	bfs(Y-1, 0);
-
-	//count shortest path length
-	int cnt = countPathLen(Y-1, 0, 0, X-1);
-	printf("%d\n", cnt);
-	
-	return 0;
+    scanf("%d %d", &Y, &X);
+    
+    for (int j = 0; j < Y; j++)
+        for (int i = 0; i < X; i++)
+            scanf("%d", &board[j][i]);
+            
+    if(Y == 1 && X == 1){//do not forget this base case!!
+        printf("0\n");
+        return 0;
+    }
+ 
+    for(int j = 0 ; j < Y ; j++)
+        for (int i = 0; i < X; i++) {
+            bDiscovered[j][i] = false;
+            bVisited[j][i] = false;
+        }
+ 
+    //convert maze to graph
+    bfs(Y-1, 0);
+ 
+    //count shortest path length
+    int cnt = 0;
+    int tempY = 0, tempX = X-1;
+    int j, i;
+    while (true) {
+        cnt++;
+        if (prePoint[tempY][tempX].y == Y - 1
+            && prePoint[tempY][tempX].x == 0)
+            break;
+        j = prePoint[tempY][tempX].y;
+        i = prePoint[tempY][tempX].x;;
+        tempY = j;
+        tempX = i;
+    }
+    printf("%d\n", cnt);
+    
+    return 0;
 }
