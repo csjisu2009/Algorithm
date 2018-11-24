5579102b4e7ccbc2115f9994c4c0ca638a76257e
JISU SHIN <csjisu2009@gmail.com>
2018-11-24 16:35:58 +0900

    str6Solved

---
+++ Algorithm/course7/6_str6.cpp
@@ -0,0 +1,41 @@
+//time   : 0h 10m
+ 
+/*review : 
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 1000
+
+void printStrInclude(char* a, char* b, int aSize, int bSize){
+    int bInclude = false;
+    for(int i = 0 ; i < aSize ; i++){
+        for(int j = 0 ; j < bSize ; j++){
+            if(a[i+j] != b[j])
+                break;
+            if(j == bSize-1 && a[i+j] == b[j]){
+                bInclude = true;
+                break;
+            }
+        }
+        if(bInclude)
+            break;
+    }
+    if(bInclude)
+        printf("YES\n");
+    else
+        printf("NO\n");
+    return;
+}
+
+int main(){
+    char strA[MAX+1];
+    char strB[MAX+1];
+    scanf("%[^\n]", &strA);//gets(strA);
+    scanf(" %[^\n]", &strB);//gets(strB);
+    int lenA = strlen(strA);
+    int lenB = strlen(strB);
+    printStrInclude(strA, strB, lenA, lenB);
+    return 0;
+}
