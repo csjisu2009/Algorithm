5d85481b581b4443130de82337bf0e9dd89e5087
JISU SHIN <csjisu2009@gmail.com>
2018-12-26 19:33:53 +0900

    Test_10_JJSolved

---
+++ Algorithm/Test/Test_10_JJ.cpp
@@ -0,0 +1,42 @@
+//time   : 0h 20m
+
+/*review : 1. naive method : 60 points
+
+           2. do not forget "return" !! in "if(r == 0)" logic 
+
+*/
+
+#include <stdio.h>
+
+char str[27];
+bool bChar[27];//a(97) ~ z(122); used - true, unused - false
+int numChar, numElement;
+
+void dictionaryStr(int r){
+    if(r == 0){
+        for(int i = 0 ; i < numElement ; i++)
+            printf("%c", str[i]);
+        printf("\n");
+        return;//do not forget this line!! else "14 1" case is "Time Limit Exceeded"
+    }
+    for(int i = 0 ; i < numChar ; i++){
+        if(!bChar[i]){
+            str[numElement-r] = i+97;//str[i] = i+97;
+            bChar[i] = true;
+            dictionaryStr(r-1);
+            str[numElement-r] = 0;//str[i] = 0;
+            bChar[i] = false;
+        }
+    }
+    return;
+}
+
+int main(){
+    scanf("%d %d", &numChar, &numElement);
+    for(int i = 0 ; i < numChar ; i++)
+        bChar[i] = false;
+
+    dictionaryStr(numElement);
+
+    return 0;
+}
---
+++ Algorithm/course9/1_numberbox.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void binSearch(){
+    
+}
+
+int main(){
+    return 0;
+}
