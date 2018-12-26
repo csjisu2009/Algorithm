491579e713685467c34d901557411a89766eee3a
JISU SHIN <csjisu2009@gmail.com>
2018-12-26 12:54:55 +0900

    ValueOfBracketsSolved

--- Algorithm/course8/7_ValueOfBrackets.cpp
+++ Algorithm/course8/7_ValueOfBrackets.cpp
@@ -1,3 +1,88 @@
+//time   : 3h 0m
+
+/*review : 1. 1 kinds of method
+            1) complicate method : 80 points..
+            2) add number at every step method
+
+           2. sometimes you should think the whole process again!!
+
+*/
+
+// 2) add number at every step method
+#include <iostream>
+#include <stack>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+#define MAX 30
+
+char str[MAX + 1];
+int sum = 0;//psum is a part of sum
+
+stack<char> sBrackets;
+
+bool checkGrammer() {
+	int size = strlen(str);
+	for (int i = 0; i < size; i++) {
+		if (str[i] == '(' || str[i] == '[')
+			sBrackets.push(str[i]);
+		else {
+			if (sBrackets.empty())
+				return false;
+			else if (str[i] == ')' && sBrackets.top() == '[')
+				return false;
+			else if (str[i] == ']' && sBrackets.top() == '(')
+				return false;
+			else
+				sBrackets.pop();
+		}
+	}
+	return sBrackets.empty();
+}
+
+void calcBrackets() {
+	int size = strlen(str);
+	int num = 1;
+	for (int i = 0; i < size; i++) {
+		if (str[i] == '(') {
+			sBrackets.push('(');
+			num *= 2;
+			if (i + 1 < size && str[i + 1] == ')')
+				sum += num;
+		}
+		else if (str[i] == '[') {
+			sBrackets.push('[');
+			num *= 3;
+			if (i + 1 < size && str[i + 1] == ']')
+				sum += num;
+		}
+		else if (str[i] == ')') {
+			sBrackets.pop();
+			num /= 2;
+		}
+		else if (str[i] == ']') {
+			sBrackets.pop();
+			num /= 3;
+		}
+	}
+}
+
+int main() {
+	cin >> str;
+	if (!checkGrammer()) {
+		cout << '0' << endl;
+		return 0;
+	}
+	calcBrackets();
+	printf("%d\n", sum);
+	return 0;
+}
+
+
+//80 points
+/*
 #include <iostream>
 #include <stack>
 #include <stdio.h>
@@ -48,19 +133,28 @@ void calcBrackets() {
 	Bracket bTop;
 	int size = strlen(str);
 	for (int i = 0; i < size; i++) {
+		//push
 		if (str[i] == '(' || str[i] == '[')
 			sB.push(Bracket(str[i]));
+		//pop
 		else {
 			bTop = sB.top();
 			sB.pop();
+
+			//when stack of Bracket is empty
+			//calculate psum
 			if (!sB.empty()) {
 				psum *= bTop.number;
+				//add psum to sum, when bracket start with '(' or '['
 				if (i + 1 < size && (str[i + 1] == '(' || str[i + 1] == '[')) {
 					sum += psum;
 					psum = 1;
 				}
 			}
+			//when stack of Bracket is empty
+			//calculate sum
 			else {
+				//when there are just one pair of bracket    ; e) ()[]()
 				if (psum == 1) {
 					psum *= bTop.number;
 					sum += psum;
@@ -97,3 +191,7 @@ int main() {
 	cout << calcSum() << endl;
 	return 0;
 }
+*/
+
+//(([](([]))))
+//60
--- Algorithm/course9/2_snail.cpp
+++ Algorithm/course9/2_snail.cpp
@@ -1,24 +1,43 @@
+
+
+//naive method
+
 #include <iostream>//#include <stdio.h>
 
 using namespace std;
 
 enum Direction{UP, DOWN};
-long long int move[2];
+int moveArr[2];
 
 int main(){
-    long long int a, b, v, hMove = 0;
-    long long int day = -1;
+    int a, b, v, hMove = 0;
+    int day = 1;// yamae 
+    int day = 0;
     cin >> a >> b >> v;//scanf("%d %d %d", &a, &b, &v);
-    move[UP] = a;
-    move[DOWN] = b;
-
-    while(hMove <= v){
-        day++;
-        hMove += move[UP];
-        if(v < hMove) break;
-        hMove -= move[DOWN];
-        
+    moveArr[UP] = a;
+    moveArr[DOWN] = b;
+    
+    while(hMove < v){
+       day++; 
+       hMove += moveArr[UP];
+       if(v <= hMove) break;
+       hMove -= moveArr[DOWN];
+       //day++;
     }
     cout << day << endl;//printf("%d\n", day);
     return 0;
 }
+
+
+/*
+2
+1
+
+3
+2
+
+4
+3
+
+5
+*/
